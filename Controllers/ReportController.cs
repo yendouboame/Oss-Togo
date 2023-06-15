b2afd19f0b4029a47895bb41c34ce2b26a81ad62
Obed DEGBO <degboobed@outlook.com>
2023-06-15 10:50:17 +0000

    Push avant 'datetimepicker'

---
+++ Controllers/ReportController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolidarityFund.Controllers
+{
+    public class ReportController : BaseController
+    {
+        public IActionResult PriestReportFilter()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
